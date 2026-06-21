ca0ed96902ccf18f5e3e83d410dff14792009a44
Praveen <pkumarprajapati007@gmail.com>
2026-06-21 11:41:50 +0530

    Created  user, event, booking db models

---
+++ src/models/bookings.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    event: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    bookingDate: { type: Date, default: Date.now },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+const Booking = mongoose.model('Booking', eventSchema);
+export default Booking;
